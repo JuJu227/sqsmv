ff103e6c9035608768702c83f46b91c864051a59
Carl Jones <cjones@uplandsoftware.com>
2022-01-19 18:16:22 -0500

    upgraded the sdk and and remove the need for nested waitgroups

--- main.go
+++ main.go
@@ -1,123 +1,115 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"log"
 	"os"
 	"sync"
 
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/sqs"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/sqs"
+	"github.com/aws/aws-sdk-go-v2/service/sqs/types"
 )
+
+type client struct {
+	session *sqs.Client
+	rmi     *sqs.ReceiveMessageInput
+	dest    string
+}
+
+func newClient(src string, dest string) *client {
+	cfg, err := config.LoadDefaultConfig(context.TODO())
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+
+	return &client{
+		session: sqs.NewFromConfig(cfg),
+		rmi: &sqs.ReceiveMessageInput{
+			QueueUrl:              aws.String(src),
+			MaxNumberOfMessages:   10,
+			WaitTimeSeconds:       0,
+			MessageAttributeNames: []string{"ALL"},
+		},
+		dest: dest,
+	}
+}
+
+//transferMessages loops, transferring a number of messages from the src to the dest at an interval.
+func (c *client) transferMessages(ctx context.Context, wgOuter *sync.WaitGroup) error {
+
+	defer wgOuter.Done()
+
+	resp, err := c.session.ReceiveMessage(ctx, c.rmi)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	lastMessageCount := len(resp.Messages)
+	log.Printf("received %v messages...", len(resp.Messages))
+
+	smsg := make([]types.SendMessageBatchRequestEntry, 0, lastMessageCount)
+	dmsg := make([]types.DeleteMessageBatchRequestEntry, 0, lastMessageCount)
+
+	//loading batch
+	for _, m := range resp.Messages {
+		smsg = append(smsg, types.SendMessageBatchRequestEntry{
+			Id:          m.MessageId,
+			MessageBody: m.Body,
+		})
+
+		dmsg = append(dmsg, types.DeleteMessageBatchRequestEntry{
+			Id:            m.MessageId,
+			ReceiptHandle: m.ReceiptHandle,
+		})
+	}
+
+	_, err = c.session.SendMessageBatch(ctx, &sqs.SendMessageBatchInput{
+		Entries:  smsg,
+		QueueUrl: aws.String(c.dest),
+	})
+	if err != nil {
+		log.Println(err.Error())
+		return err
+	}
+
+	_, err = c.session.DeleteMessageBatch(ctx, &sqs.DeleteMessageBatchInput{
+		Entries:  dmsg,
+		QueueUrl: c.rmi.QueueUrl,
+	})
+	return err
+}
 
 func main() {
 	src := flag.String("src", "", "source queue")
 	dest := flag.String("dest", "", "destination queue")
-	clients := flag.Int("clients", 1, "number of clients")
+	nClients := flag.Int("clients", 1, "number of clients")
 	flag.Parse()
 
-	if *src == "" || *dest == "" || *clients < 1 {
+	if *src == "" || *dest == "" {
 		flag.Usage()
 		os.Exit(1)
 	}
 
+	if *nClients < 1 {
+		*nClients = 1
+	}
+
 	log.Printf("source queue : %v", *src)
 	log.Printf("destination queue : %v", *dest)
-	log.Printf("number of clients : %v", *clients)
+	log.Printf("number of clients : %v", *nClients)
 
-	// enable automatic use of AWS_PROFILE like awscli and other tools do.
-	opts := session.Options{
-		SharedConfigState: session.SharedConfigEnable,
-	}
-
-	sess, err := session.NewSessionWithOptions(opts)
-	if err != nil {
-		panic(err)
-	}
-
-	maxMessages := int64(10)
-	waitTime := int64(0)
-	messageAttributeNames := aws.StringSlice([]string{"All"})
-
-	rmin := &sqs.ReceiveMessageInput{
-		QueueUrl:              src,
-		MaxNumberOfMessages:   &maxMessages,
-		WaitTimeSeconds:       &waitTime,
-		MessageAttributeNames: messageAttributeNames,
-	}
+	client := newClient(*src, *dest)
 
 	var wg sync.WaitGroup
-	for i := 1; i <= *clients; i++ {
+	for i := 1; i <= *nClients; i++ {
 		wg.Add(i)
-		go transferMessages(sess, rmin, dest, &wg)
+		go client.transferMessages(context.TODO(), &wg)
 	}
 	wg.Wait()
 
 	log.Println("all done")
-}
-
-//transferMessages loops, transferring a number of messages from the src to the dest at an interval.
-func transferMessages(theSession *session.Session, rmin *sqs.ReceiveMessageInput, dest *string, wgOuter *sync.WaitGroup) {
-	client := sqs.New(theSession)
-
-	lastMessageCount := int(1)
-
-	defer wgOuter.Done()
-
-	// loop as long as there are messages on the queue
-	for {
-		resp, err := client.ReceiveMessage(rmin)
-
-		if err != nil {
-			panic(err)
-		}
-
-		if lastMessageCount == 0 && len(resp.Messages) == 0 {
-			// no messages returned twice now, the queue is probably empty
-			log.Printf("done")
-			return
-		}
-
-		lastMessageCount = len(resp.Messages)
-		log.Printf("received %v messages...", len(resp.Messages))
-
-		var wg sync.WaitGroup
-		wg.Add(len(resp.Messages))
-
-		for _, m := range resp.Messages {
-			go func(m *sqs.Message) {
-				defer wg.Done()
-
-				// write the message to the destination queue
-				smi := sqs.SendMessageInput{
-					MessageAttributes: m.MessageAttributes,
-					MessageBody:       m.Body,
-					QueueUrl:          dest,
-				}
-
-				_, err := client.SendMessage(&smi)
-
-				if err != nil {
-					log.Printf("ERROR sending message to destination %v", err)
-					return
-				}
-
-				// message was sent, dequeue from source queue
-				dmi := &sqs.DeleteMessageInput{
-					QueueUrl:      rmin.QueueUrl,
-					ReceiptHandle: m.ReceiptHandle,
-				}
-
-				if _, err := client.DeleteMessage(dmi); err != nil {
-					log.Printf("ERROR dequeueing message ID %v : %v",
-						*m.ReceiptHandle,
-						err)
-				}
-			}(m)
-		}
-
-		// wait for all jobs from this batch...
-		wg.Wait()
-	}
 }
